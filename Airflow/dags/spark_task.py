0f48a6dce8e19e8387ea2e0c0f90be0a021cfc1b
Triệu Hoàng Hiệp <youthing1111@gmail.com>
2025-06-18 17:14:12 +0100

    update spark task and config

--- Airflow/dags/spark_task.py
+++ Airflow/dags/spark_task.py
@@ -1,11 +1,15 @@
 from pyspark.sql import SparkSession
 from pyspark import SQLContext
 from pyspark.sql import functions as F
+from pyspark.sql.functions import desc
 from pyspark.sql.window import Window
 import boto3
 import pandas as pd
 from io import StringIO
-from io import BytesIO
+from pyspark.sql.functions import pandas_udf
+from pyspark.sql.functions import PandasUDFType
+from pyspark.sql.types import DoubleType, StructField
+from pyspark.sql import pandas as ps
 
 spark = SparkSession \
     .builder \
@@ -20,18 +24,29 @@ s3 = boto3.resource('s3',
 
 object = s3.Bucket("mlflow-artifacts").Object("acb_stock_202506151415.csv").get()['Body'].read()
 
-
 s = str(object,'utf-8')
 data = StringIO(s)
 df = pd.read_csv(data)
 
 df_sp = spark.createDataFrame(df)
 
-w = (Window().partitionBy("date").rowsBetween(-9, 0))
+w = (Window().orderBy(desc("date")).rowsBetween(0, 9))
 df_sp = df_sp.withColumn('close_sma', F.avg("close").over(w))
 
+@pandas_udf(DoubleType())
+def ewm_udf_12(s: pd.Series) -> pd.Series:
+    return s.ewm(span=12).mean()  # Adjust the span as needed
 
-print(df_sp.limit(5).show() )
+@pandas_udf(DoubleType())
+def ewm_udf_26(s: pd.Series) -> pd.Series:
+    return s.ewm(span=26).mean()  # Adjust the span as needed
+
+df_sp = df_sp.withColumn('close_ema12', ewm_udf_12(F.col("close")))
+df_sp = df_sp.withColumn('close_ema26', ewm_udf_26(F.col("close")))
+df_sp = df_sp.withColumn('MACD', df_sp.close_ema12 - df_sp.close_ema26 )
+
+
+print(df_sp.collect()[:5])
 
 csv_buffer = StringIO()
 df.to_csv(csv_buffer)
