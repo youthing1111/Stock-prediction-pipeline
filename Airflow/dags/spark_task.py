a9ee92f299181025b2be9a157c6604efc32dcad8
Triệu Hoàng Hiệp <youthing1111@gmail.com>
2025-06-22 14:48:55 +0100

    update spark docker and task

--- Airflow/dags/spark_task.py
+++ Airflow/dags/spark_task.py
@@ -30,7 +30,7 @@ df = pd.read_csv(data)
 
 df_sp = spark.createDataFrame(df)
 
-w = (Window().orderBy(desc("date")).rowsBetween(0, 9))
+w = (Window().orderBy(desc("date")).rowsBetween(0, 8))
 df_sp = df_sp.withColumn('close_sma', F.avg("close").over(w))
 
 @pandas_udf(DoubleType())
@@ -45,6 +45,42 @@ df_sp = df_sp.withColumn('close_ema12', ewm_udf_12(F.col("close")))
 df_sp = df_sp.withColumn('close_ema26', ewm_udf_26(F.col("close")))
 df_sp = df_sp.withColumn('MACD', df_sp.close_ema12 - df_sp.close_ema26 )
 
+window_spec = (Window().orderBy(desc("date")).rowsBetween(0,14 - 1))
+# Calculate the best low and high
+df_sp = df_sp.withColumn("best_low", F.min("close").over(window_spec))
+df_sp = df_sp.withColumn("best_high", F.max("close").over(window_spec))
+df_sp = df_sp.withColumn(
+    "fast_k",
+    100 * (F.col("close") - F.col("best_low")) / (F.col("best_high") - F.col("best_low")+0.0001)
+)
+fast_k_window = Window.orderBy(desc("date")).rowsBetween(0, 2)
+# Calculate fast %D
+df_sp = df_sp.withColumn("fast_d", F.round(F.avg("fast_k").over(fast_k_window), 2))
+# Calculate slow %K and slow %D
+df_sp = df_sp.withColumn("slow_k", F.col("fast_d"))
+df_sp = df_sp.withColumn("slow_d", F.round(F.avg("slow_k").over(fast_k_window), 2))
+
+df_sp = df_sp.withColumn("LW", 100 * (F.col("high") - F.col("close")) / (F.col("high") - F.col("low")+0.0001))
+diff_window = (Window().orderBy(desc("date")))
+df_sp = df_sp.withColumn('close_t+1', F.lag('close').over(diff_window))
+df_sp = df_sp.withColumn('Target', F.when(F.col('close_t+1')>F.col('close'),1).otherwise(0))
+df_sp = df_sp.withColumn('acc_dist',F.when(F.col('high') != F.col('low'),((F.col("close") - F.col("low") - F.col("high") + F.col("close")) / (F.col("high") - F.col("low")))*F.col("volume")).otherwise(0))
+@pandas_udf(DoubleType())
+def acc_dist_ema_9(s: pd.Series) -> pd.Series:
+    return s.ewm(com=9).mean()  # Adjust the span as needed
+df_sp = df_sp.withColumn('acc_dist_ema_9', acc_dist_ema_9(F.col("acc_dist")))
+
+df_sp = df_sp.withColumn("obv", F.lit(0))
+window_spec = Window.orderBy("date")
+# Calculate the OBV
+df_sp = df_sp.withColumn("prev_close", F.lag('close').over(window_spec))
+df_sp = df_sp.withColumn("prev_obv", F.lag("volume").over(window_spec))
+df_sp = df_sp.withColumn(
+    "obv",
+    F.when(F.col('close') > F.col("prev_close"), F.col('volume') + F.col("prev_obv"))
+        .when(F.col('close') < F.col("prev_close"), F.col("prev_obv") - F.col('volume'))
+        .otherwise(F.col("prev_obv"))
+)
 
 print(df_sp.collect()[:5])
 
